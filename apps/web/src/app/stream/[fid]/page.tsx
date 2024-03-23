d048946e795f378170f4fc96413eaaaf82f7049c
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-23 17:33:39 +0100

    make streamer page server rendered

--- apps/web/src/app/stream/[fid]/page.tsx
+++ apps/web/src/app/stream/[fid]/page.tsx
@@ -1,37 +1,36 @@
-"use client";
-
 import ColorfulText from "@components/ColorText";
 import { DemoPlayer } from "@components/Player";
 import { Alert, AlertDescription, AlertTitle } from "@components/ui/alert";
-import { usePrivy } from "@privy-io/react-auth";
-import { useQuery } from "@tanstack/react-query";
+import { baseURL } from "@lib/variables";
 import { TvIcon } from "lucide-react";
+import { neynar } from "src/app/api/neynar";
 
-const PageWithStreamPlayer = () => {
-  const { user } = usePrivy();
+type StreamerPageProps = {
+  params: { fid: string };
+};
 
-  const { data } = useQuery({
-    queryKey: ["playback"],
-    queryFn: () =>
-      fetch("/api/stream/playback/" + user?.farcaster?.fid).then((res) =>
-        res.json()
-      ),
-  });
+const PageWithStreamPlayer = async ({ params: { fid } }: StreamerPageProps) => {
+  const playback = await fetch(baseURL + "/api/stream/playback/" + fid).then(
+    (res) => res.json()
+  );
+  const farcasterUser = await neynar
+    .fetchBulkUsers([parseInt(fid)])
+    .then((res) => res.users[0]);
 
   return (
     <div className="gap-2 flex flex-col">
-      {user?.farcaster?.displayName && (
+      {farcasterUser && (
         <Alert>
           <TvIcon className="h-4 w-4" />
           <AlertTitle>Thank for tuning in!</AlertTitle>
           <AlertDescription>
             You're currently watching{" "}
-            <ColorfulText element="span" text={user?.farcaster?.displayName} />
+            <ColorfulText element="span" text={farcasterUser.display_name} />
             's stream.
           </AlertDescription>
         </Alert>
       )}
-      {data && <DemoPlayer playback={data} />}
+      {playback && <DemoPlayer playback={playback} />}
     </div>
   );
 };
